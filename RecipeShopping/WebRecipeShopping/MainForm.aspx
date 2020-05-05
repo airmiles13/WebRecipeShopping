52345c164c86ea28073a76b25f2bb3f995d34c90
airmiles13 <alex.m.miles@gmail.com>
2020-05-05 07:43:30 -0400

    Initial Commit. Basic Web posting working.

---
+++ RecipeShopping/WebRecipeShopping/MainForm.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="WebRecipeShopping.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Add Ingredient:<br />
+            <asp:TextBox ID="TextBoxIng" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnSubmit" runat="server" OnClick="BtnSubmit_Click" Text="Submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ RecipeShopping/WebRecipeShopping/MainForm.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WebRecipeShopping
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server = tcp:milesaheadproduction.database.windows.net,1433; Initial Catalog = maprecipe; Persist Security Info = False; User ID = mapadmin; Password =3Cx^r^?Nmk^Mm^q@; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+
+            {
+                SqlCommand command = new SqlCommand("AddIngredient", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ingredient", TextBoxIng.Text);
+                connection.Open();
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
